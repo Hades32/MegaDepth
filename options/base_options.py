69558b64d65367b98faad6d2b6766c02a28d4747
Martin Rauscher <hades32@gmail.com>
2018-06-24 22:41:37 +0200

    next try

--- options/base_options.py
+++ options/base_options.py
@@ -36,14 +36,12 @@ class BaseOptions():
         self.parser.add_argument('--use_dropout', action='store_true', help='use dropout for the generator')
         self.parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
 
-        self.parser.add_argument('-f', type=str, default='whatever', help='we ignore stuff Jupyter passes...')
-
         self.initialized = True
 
     def parse(self):
         if not self.initialized:
             self.initialize()
-        self.opt = self.parser.parse_known_args([])
+        self.opt = self.parser.parse_args([])
         self.opt.isTrain = self.isTrain   # train or test
 
         str_ids = self.opt.gpu_ids.split(',')
