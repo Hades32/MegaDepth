a00d9f57081e2c42226223d7cfb082eb27ee8cbb
Martin Rauscher <hades32@gmail.com>
2018-06-24 22:36:37 +0200

    in Jupyter we don't get Parameters anyway

--- options/base_options.py
+++ options/base_options.py
@@ -43,7 +43,7 @@ class BaseOptions():
     def parse(self):
         if not self.initialized:
             self.initialize()
-        self.opt = self.parser.parse_known_args()
+        self.opt = self.parser.parse_known_args([])
         self.opt.isTrain = self.isTrain   # train or test
 
         str_ids = self.opt.gpu_ids.split(',')
